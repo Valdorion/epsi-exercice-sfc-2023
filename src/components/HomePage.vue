eb0e5c66fe80d927d6fc2afb3ff0227f4bf15192
Valdorion <valdorion.kalagia@gmail.com>
2024-02-20 11:37:37 +0100

    sombre merde

---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '../assets/Style/homePage.css'
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="">
+  </div>
+</template>
